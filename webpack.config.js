09546225162dc37c25c99eb3a32fdb47efe26d8b
Dave Cusatis <davecusatis1@gmail.com>
2018-04-29 16:25:36 -0700

    now with live reloading :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,28 @@
+var path = require('path');
+var fs = require('fs');
+
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+  mode: 'development',
+  entry: path.resolve(__dirname,'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+          test: /\.tsx?$/,
+          use: [ "ts-loader"]
+      },
+    ]
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
-
-    devServer: {
-      contentBase: __dirname,
-      compress: true,
-      port: 8080,
-      host: "localhost",
-      https: true,
-      index: __dirname + "/index.html",
-    }
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    index: path.join(__dirname, 'public/index.html'),
+  }
 };
